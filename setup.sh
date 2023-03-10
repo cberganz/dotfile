a234e8ca7dcd2fc66cb67e50d62224bef2aa1c71
Charles Berganza <80924963+cberganz@users.noreply.github.com>
2023-03-10 02:14:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,48 @@
 #!/bin/bash
 
-#sudo apt-get update && sudo apt-get upgrade -y
-#sudo apt-get install -y vim git zsh curl
-#sudo apt-get install -y clang make
-#sudo apt-get install -y libbsd-dev
-#sudo apt remove unattended-upgrades
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt-get update && sudo apt-get upgrade -y
 
+sudo apt-get install -y git \
+                        build-essential \
+                        zsh \
+                        tree \
+                        curl \
+                        terminator \
+                        clang \
+                        make \
+                        cmake \
+                        pkg-config \
+                        libfreetype6-dev \
+                        libfontconfig1-dev \
+                        libxcb-xfixes0-dev \
+                        libxkbcommon-dev \
+                        python3
+
+sudo apt remove unattended-upgrades
+
+# Install OhMyZsh!
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Rust compiler
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+rustup override set stable
+rustup update stable
+
+# Intall Alacritty
+git clone https://github.com/alacritty/alacritty.git && cd alacritty
+cargo build --release
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+sudo cp target/release/alacritty /usr/local/bin
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+mkdir -p ${ZDOTDIR:-~}/.zsh_functions
+echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
+cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
+gsettings set org.gnome.desktop.default-applications.terminal exec /usr/local/bin/alacritty
+
+# Worspace handling on multiple screens
 gsettings set org.gnome.mutter workspaces-only-on-primary false
 
 # SETUP AUTOLOAD
